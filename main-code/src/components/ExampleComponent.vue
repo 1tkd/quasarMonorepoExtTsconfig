2a698a376b6aa85cab9461ca5d40dc65a8296194
1tkd <1tkd.git@gmail.com>
2023-04-01 01:12:25 +0900

    init

---
+++ main-code/src/components/ExampleComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>mainComp {{ func1() }}</p>
+    <LIbComp1></LIbComp1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import LIbComp1 from 'lib/components/LibComp.vue'
+import {func1} from  'lib/components/utilcode'
+
+
+</script>
